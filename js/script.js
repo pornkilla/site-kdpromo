626daa1ab0109b26114bd69e77ae63a4f8eb8951
Pornkilla <pornkilla@gmail.com>
2019-10-14 00:36:36 +0300

    sendmail update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-$('#form-one').form({});
-
-$('#form-two').form({
-    inline: true,
+$('.ui.form.callback').form({
+inline: true,
+    on: 'blur',
+    transition: 'fade down',
+    onSuccess: nowIsValid,
     fields: {
         fieldname: {
             identifier: 'fieldname',
@@ -27,45 +28,83 @@ $('#form-two').form({
                 type: 'minLength[5]',
                 prompt: 'Мы не уверены, что такие короткие номера вообще бывают'
             }]
+        }       
+    }
+});
+
+
+$('.ui.form.contact').form({
+    inline: true,
+    on: 'blur',
+    transition: 'fade down',
+    onSuccess: nowIsValid,    
+    fields: {
+        fieldname: {
+            identifier: 'fieldname',
+            rules: [{
+                    type: 'empty',
+                    prompt: 'Назовите себя, пожалуйста'
+                },
+                {
+                    type: 'minLength[2]',
+                    prompt: 'Введите хотя бы две буквы'
+                }
+            ]
+        },
+        fieldphone: {
+            identifier: 'fieldphone',
+            rules: [{
+                type: 'empty',
+                prompt: 'Пожалуйста, укажите телефон для связи'
+            }, {
+                type: 'number',
+                prompt: 'Допускаются только цифры'
+            }, {
+                type: 'minLength[5]',
+                prompt: 'Мы не уверены, что такие короткие номера вообще бывают'
+            }]
+        },
+        terms: {
+            identifier: 'terms',
+            rules: [{
+                type: 'checked',
+                prompt: 'Поле обязательно для подтверждения'
+            }]
         }
     }
 });
 
-$('#form-three').form({});
+function reportSuccess(data) {
+     var targetSuccess = $(".form.contact .success");     
+     targetSuccess.removeClass("hidden");
+     targetSuccess.html('<p class="centered">' + data.text + '</p>').slideDown();
+     $(".form.contact input[type=text], .form.contact input[type=tel], .form.contact textarea").val('');
+}
 
-$('#form-four').form({});
-
-//Ajax form submit
-$(".form button").click(function () {
-    var selectedForm = this.closest('.form');
-    var proceed = true;
-
-    if (proceed) //everything looks good! proceed...
-    {
-        //get input field values data to be sent to server
-        post_data = {
-            'user_name': $selectedForm.find('input[name=fieldname]').val(),
-            // 'user_email'    : $('input[name=email]').val(), 
-            // 'country_code': $('input[name=phone1]').val(),
-            'phone_number': $selectedForm.find('input[name=fieldphone]').val(),
-            // 'subject'       : $('select[name=subject]').val(), 
-            'msg': $selectedForm.find('textarea[name=message]').val()
-        };
-
-        //Ajax post data to server
-        $.post('sendmail.php', post_data, function (response) {
-            if (response.type == 'error') { //load json data from server and output message     
-                output = '<div class="error">' + response.text + '</div>';
-            } else {
-                output = '<div class="success">' + response.text + '</div>';
-                //reset values in all input fields
-                $(".form input[required=true], .form textarea").val('');
+function nowIsValid() {
+    var myform = $('.ui.form.contact');        
+    $.ajax({
+        type: 'POST',
+        url: '../sendmail.php',
+        data: myform.serialize(),
+        dataType: 'json',
+        success: function (data) {
+            if (data.type == 'error') {
+                var targetError = $(".form.contact .error");
+                output = '<p>' + data.text + '</p>';    
+                targetError.html(output).slideDown();
             }
-            $(".form #contact_results").hide().html(output).slideDown();
-        }, 'json');
-    }
+            if (data.type == 'done') {
+                reportSuccess(data);
+            }
+        }      
+    });
+}
+
+// stop the form from submitting normally 
+$('.ui.form.contact').submit(function () {
+    return false;
 });
-// ----------
 
 var videoItem = $('#spaceforvideo video');
 var videoMenuItem = $('#spaceforvideo .menu .item');
@@ -98,3 +137,4 @@ videoMenuItem.tab({
     context: 'parent'
 });
 $('.ui.accordion').accordion();
+$('.ui.checkbox').checkbox();
--- sendmail.php
+++ sendmail.php
@@ -1,70 +1,51 @@
 <?php
-if($_POST) {
-    $to_email = "bomber@sampo.ru"; //Recipient email, Replace with own email here
-    $from_email = "info@karelianhouse.ru"; //From email, most emails won't work without it.
-  
-    //check if its an ajax request, exit if not
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-        
-        $output = json_encode(array( //create JSON data
-            'type'=>'error', 
-            'text' => 'Sorry Request must be Ajax POST'
-        ));
-        die($output); //exit script outputting json data
-    } 
-    
-    //Sanitize input data using PHP filter_var().
-    $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
-    // $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
-    // $country_code   = filter_var($_POST["country_code"], FILTER_SANITIZE_NUMBER_INT);
-    $phone_number   = filter_var($_POST["phone_number"], FILTER_SANITIZE_NUMBER_INT);
-    // $subject        = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-    $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
-    
-    //additional php validation
-    if(strlen($user_name)<2){ // If length is less than 2 it will output JSON error.
-        $output = json_encode(array('type'=>'error', 'text' => 'В имени слишком мало символов'));
-        die($output);
-    }
-    // if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)){ //email validation
-    //     $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
-    //     die($output);
+$recipient_email    = "pornkilla@gmail.com"; //recepient
+$from_email         = "info@karelianhouse.ru"; //from email using site domain.
+$subject            = "Новое сообщение с промо-страницы";
+
+
+if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    die('Sorry Request must be Ajax POST'); //exit script
+}
+
+if($_POST){
+
+    $sender_name    = filter_var($_POST["fieldname"], FILTER_SANITIZE_STRING); //capture sender name
+    $sender_phone   = filter_var($_POST["fieldphone"], FILTER_SANITIZE_STRING); //capture sender email
+    $message        = filter_var($_POST["fieldmessage"], FILTER_SANITIZE_STRING); //capture message
+    $message .= "\n Имя: " . $sender_name;
+    $message .= "\n Телефон: " . $sender_phone;
+
+    //php validation
+    // if(strlen($sender_name)<2){ // If length is less than 2 it will output JSON error.
+    //     print json_encode(array('type'=>'error', 'text' => 'Имя не должно быть короче 2-х символов'));
+    //     exit;
     // }
-    // if(!filter_var($country_code, FILTER_VALIDATE_INT)){ //check for valid numbers in country code field
-    //     $output = json_encode(array('type'=>'error', 'text' => 'В коде страны ошибка'));
-    //     die($output);
-    // }
-    if(!filter_var($phone_number, FILTER_SANITIZE_NUMBER_FLOAT)){ //check for valid numbers in phone number field
-        $output = json_encode(array('type'=>'error', 'text' => 'Только цифры в номере телефона!'));
-        die($output);
-    }
-    // if(strlen($subject)<3){ //check emtpy subject
-    //     $output = json_encode(array('type'=>'error', 'text' => 'Subject is required'));
-    //     die($output);
+    // if(!filter_var($sender_phone, FILTER_VALIDATE_EMAIL)){ //email validation
+    //     print json_encode(array('type'=>'error', 'text' => 'Пожалуйста, укажите корректный адрес электронной почты.'));
+    //     exit;
     // }
     // if(strlen($message)<3){ //check emtpy message
-    //     $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
-    //     die($output);
+    //     print json_encode(array('type'=>'error', 'text' => 'Сообщение не содержит какой-либо ценной информации и слишком лаконично. Попробуйте добавить содержания.'));
+    //     exit;
     // }
-    
-    //email body
-    $message_body = $message."\r\n\r\n-".$user_name."\r\nТелефон : ".$phone_number;
-    
-    //proceed with PHP email.
-    $headers = 'From: '.$from_email.'' . "\r\n" .
-    'Reply-To: '.$from_email.'' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    
-    $send_mail = mail($to_email, $subject, $message_body, $headers);
-    
-    if(!$send_mail)
+
+
+    $boundary = md5("karelianhouse.ru");
+
+    $headers = "From:".$from_email."\r\n".
+     "Reply-To: ".$from_email. "\n" .
+     "X-Mailer: PHP/" . phpversion();
+     $body = $message;
+
+    $sentMail = mail($recipient_email, $subject, $body, $headers);
+    if($sentMail) //output success or failure messages
     {
-        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
-        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
-        die($output);
+        print json_encode(array('type'=>'done', 'text' => 'Сообщение отправлено, благодарим!'));
+        exit;
     }else{
-        $output = json_encode(array('type'=>'message', 'text' => 'Уважаемый(ая) '.$user_name .', Ваш запрос был отправлен!'));
-        die($output);
+        print json_encode(array('type'=>'error', 'text' => 'Ошибка отправки сообщения, проверьте конфигурацию PHP.'));
+        exit;
     }
 }
 ?>
